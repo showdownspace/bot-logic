72c9ac64538f6bedb0e4a6016c117e6c0ab3cef7
dtinth on MBP M1 <dtinth@spacet.me>
2022-04-27 22:40:06 +0700

    CitW management

--- src/code-in-the-wind.plugin.ts
+++ src/code-in-the-wind.plugin.ts
@@ -1,5 +1,7 @@
 import { definePlugin } from './bot'
+import { management } from './management'
 import { getRtSysToken } from './rt-sys'
+import { BotContext } from './types'
 
 export default definePlugin((bot) => {
   bot.handleCommand(
@@ -14,4 +16,52 @@ export default definePlugin((bot) => {
         .ok('Click the following link to open the editor:')
     },
   )
+  management(bot).handleManagementCommand(
+    'citw-unlock',
+    async (context, interaction, payload, output) => {
+      await getRoomRef(context).child('settings/acceptingSubmissions').set(true)
+      output.puts('Editor unlocked')
+    },
+  )
+  management(bot).handleManagementCommand(
+    'citw-lock',
+    async (context, interaction, payload, output) => {
+      await getRoomRef(context)
+        .child('settings/acceptingSubmissions')
+        .set(false)
+      output.puts('Editor locked')
+    },
+  )
+  management(bot).handleManagementCommand(
+    'citw-status',
+    async (context, interaction, payload, output) => {
+      const presencePromise = getRoomRef(context)
+        .child('presence')
+        .once('value')
+      const profilesPromise = getRoomRef(context)
+        .child('profiles')
+        .once('value')
+      const presence = await presencePromise
+      const profiles = await profilesPromise
+      const onlineUsers: string[] = []
+      presence.forEach((item) => {
+        const timestamp = item.val()
+        if (timestamp >= Date.now() - 60e3) {
+          const name = profiles.child(item.key!).child('name').val()
+          if (name) {
+            onlineUsers.push(name)
+          }
+        }
+      })
+      onlineUsers.sort()
+      output.puts(`Online users count: ${onlineUsers.length}`)
+      for (const user of onlineUsers) {
+        output.puts(`- ${user}`)
+      }
+    },
+  )
 })
+
+function getRoomRef(context: BotContext) {
+  return context.firebaseAdmin.database().ref('rooms/citw')
+}
--- src/management.plugin.ts
+++ src/management.plugin.ts
@@ -4,6 +4,15 @@ import { management } from './management'
 export default definePlugin((bot) => {
   bot.handleCommand('/manage', async (context, interaction, reply) => {
     const command = interaction.options.getString('command') ?? ''
+    const roleId = '964059437629333554'
+    if (
+      !interaction.guild?.members
+        .resolve(interaction.user.id)
+        ?.roles.cache.has(roleId)
+    ) {
+      reply.fail('You do not have the required role to use this command.')
+      return
+    }
     return management(bot).handleCommandInteraction(
       context,
       interaction,
