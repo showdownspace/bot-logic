2cc03c84fb83a121c9277442d80382a62f3c75f0
dtinth on MBP M1 <dtinth@spacet.me>
2022-05-08 13:40:07 +0700

    Refactor memo-tools

--- src/memo-tools.ts
+++ src/memo-tools.ts
@@ -1,6 +1,14 @@
-export class WeakMemo<K extends object, V> {
-  private readonly map = new WeakMap<K, V>()
-  constructor(private readonly factory: (key: K) => V) {}
+export interface MemoMap<K, V> {
+  has(key: K): boolean
+  get(key: K): V | undefined
+  set(key: K, value: V): void
+}
+
+export class MapMemo<K, V> {
+  constructor(
+    protected map: MemoMap<K, V>,
+    private readonly factory: (key: K) => V,
+  ) {}
   get(key: K): V {
     if (!this.map.has(key)) {
       this.map.set(key, this.factory(key))
@@ -9,14 +17,15 @@ export class WeakMemo<K extends object, V> {
   }
 }
 
-export class StrongMemo<K, V> {
-  private readonly map = new Map<K, V>()
-  constructor(private readonly factory: (key: K) => V) {}
-  get(key: K): V {
-    if (!this.map.has(key)) {
-      this.map.set(key, this.factory(key))
-    }
-    return this.map.get(key) as V
+export class WeakMemo<K extends object, V> extends MapMemo<K, V> {
+  constructor(factory: (key: K) => V) {
+    super(new WeakMap<K, V>(), factory)
+  }
+}
+
+export class StrongMemo<K extends object, V> extends MapMemo<K, V> {
+  constructor(factory: (key: K) => V) {
+    super(new Map<K, V>(), factory)
   }
 }
 
