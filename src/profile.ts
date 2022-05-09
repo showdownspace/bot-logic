caa5ab3b59a826933e5869ebf641001141714b28
dtinth on MBP M1 <dtinth@spacet.me>
2022-05-09 18:50:18 +0700

    Add type safety

--- src/profile.ts
+++ src/profile.ts
@@ -1,22 +1,38 @@
 import { User } from 'discord.js'
 import { BotContext } from './types'
 
+export interface ProfileEntity {
+  _id: string
+  discordUserId: string
+  discordTag: string
+  githubUser?: {
+    login: string
+    id: number
+    avatar_url: string
+    name: string
+  } | null
+  proposedEmail?: string | null
+  email?: string
+}
+
 export async function syncProfile(
   context: BotContext,
   user: User,
-  fieldsToSet: Record<string, any>,
+  fieldsToSet: Partial<ProfileEntity>,
 ) {
   const { db } = context
-  const result = await db.collection('profiles').findOneAndUpdate(
-    { _id: `discord${user.id}` },
-    {
-      $set: {
-        discordUserId: user.id,
-        discordTag: user.tag,
-        ...fieldsToSet,
+  const result = await db
+    .collection<ProfileEntity>('profiles')
+    .findOneAndUpdate(
+      { _id: `discord${user.id}` },
+      {
+        $set: {
+          discordUserId: user.id,
+          discordTag: user.tag,
+          ...fieldsToSet,
+        },
       },
-    },
-    { upsert: true, returnDocument: 'after' },
-  )
+      { upsert: true, returnDocument: 'after' },
+    )
   return result.value!
 }
